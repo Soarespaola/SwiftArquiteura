c01b7f5d48fe1176bce04554eb19c82777412e19
Paola Soares <paolaalcantara1@yahoo.com.br>
2023-01-02 14:39:53 -0300

    Cria pastas da arquitetura

---
+++ SwiftArquitetura/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftArquitetura
+//
+//  Created by Paola Alcantara Soares on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
